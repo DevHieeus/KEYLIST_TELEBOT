0d846921f49df682de10888547592a44ee8b4473
Ngx_Hieeus <116277261+DevHieeus@users.noreply.github.com>
2024-01-15 22:22:48 +0700

    Update and rename keylist.py to keylist.txt

--- keylist.py
+++
@@ -1,2 +0,0 @@
-keylist = ['BMouK83c0mxGaR1XGGAV', 'm3OsGnLuBbkc850G7gLa', 'j5GQmuAR7jAgHyUcfYi5', 'UOo56B6W6A7gnELlUsTi', 'ken6eB7Skn7CroKaSU9O', 'vu4TUSxMGSGN5udfMmps', 'sMAIbbFvnGCY0GRxl7fB', 'e7J1Dh0bDyWthLuZcbX0', '1oomr4iPP985LK7J1wbz', 'JVmzhZHSxl
-T7uUpI8bzW']
